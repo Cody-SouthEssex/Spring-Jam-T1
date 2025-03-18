26a51ea201fec14b7e89d97b11cae642a91f66f5
Jake <Jak3DHall@gmail.com>
2025-03-18 17:26:55 +0000

    playermovementFix

---
+++ Assets/Scripts/Dialogue/PrincessCutscene.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrincessCutscene : MonoBehaviour
+{
+    public GameObject dialogue;
+
+    public TextAsset heroTextAsset;
+    public TextAsset princessTextAsset;
+
+    private Transform player;
+    private PlayerMovement playerMovement;
+    public Transform princess;
+
+    public Dialogue currentDialogue;
+    public int cutsceneIndex = 0;
+
+    private CameraHandler cameraHandler;
+
+    public Transform playerProposePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraHandler = FindObjectOfType<CameraHandler>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentDialogue)
+        {
+            if (currentDialogue.complete)
+            {
+                cutsceneIndex += 1;
+                Cutscene();
+            }
+        }
+    }
+
+    public void Cutscene()
+    {
+        switch (cutsceneIndex)
+        {
+            default:
+                cameraHandler.focus = player;
+                playerMovement.lockMovement = false;
+                if (currentDialogue)
+                {
+                    Destroy(currentDialogue.gameObject);
+                }
+                Destroy(gameObject);
+                break;
+            case 0:
+                playerMovement.lockMovement = true;
+                player.position = playerProposePosition.position;
+                CreateDialogue(player, heroTextAsset);
+                break;
+            case 1:
+                CreateDialogue(princess, princessTextAsset);
+                cameraHandler.focus = princess;
+                break;
+        };
+    }
+
+    public void CreateDialogue(Transform target, TextAsset textAsset)
+    {
+        if (currentDialogue)
+        {
+            Destroy(currentDialogue.gameObject);
+        }
+
+        GameObject newDialogue = Instantiate(dialogue, target.position, target.rotation, target);
+        if (newDialogue.TryGetComponent(out Dialogue dialogueScript))
+        {
+            dialogueScript.textAsset = textAsset;
+            currentDialogue = dialogueScript;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Cutscene();
+    }
+}
